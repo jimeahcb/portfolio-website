1d7ff27720da16ed51be84aeca59ff25893da616
jclariz <132779635+jclariz@users.noreply.github.com>
2025-05-30 02:24:34 -0700

    Added background color and font color. Imported fonts and implemented fonts into default tailwind config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,17 +11,23 @@ const config: Config = {
     screens: {
       sm: "375px",
       md: "768px",
-      lg: "1200px",
+      lg: "1200px"
     },
     container: {
       center: true,
       padding: {
         DEFAULT: "1rem",
-        md: "2rem",
-      },
+        md: "2rem"
+      }
     },
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        serif: ["var(--font-serif)"]
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
+
 export default config;
